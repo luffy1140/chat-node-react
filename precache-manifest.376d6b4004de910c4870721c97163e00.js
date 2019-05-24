1d86601545bc85216be2d08f9d02f8e39c53df2b
Your Name <you@example.com>
2019-05-24 15:53:08 +0200

    Updates

---
+++ precache-manifest.376d6b4004de910c4870721c97163e00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbba383e147b15c07473012564eba0da",
+    "url": "/chat-node-react/index.html"
+  },
+  {
+    "revision": "cc5eb4d1f7b25a660e21",
+    "url": "/chat-node-react/static/css/main.47f5387e.chunk.css"
+  },
+  {
+    "revision": "cab0721efc0cbc87471b",
+    "url": "/chat-node-react/static/js/2.de1fb6ad.chunk.js"
+  },
+  {
+    "revision": "cc5eb4d1f7b25a660e21",
+    "url": "/chat-node-react/static/js/main.2cc7cb35.chunk.js"
+  },
+  {
+    "revision": "54b87634ea51deaeae0d",
+    "url": "/chat-node-react/static/js/runtime~main.3f665b41.js"
+  }
+]);
